f5b913865fe67dedfc41a9cb1acfe57fc51c7b95
RafesiA <whrudwls98@naver.com>
2017-10-31 11:47:34 +0900

    modify

--- src/TermProject.java
+++ src/TermProject.java
@@ -7,15 +7,14 @@ public class TermProject extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container contentPane = getContentPane();
 		contentPane.setBackground(Color.WHITE);
-		contentPane.setLayout(new GridLayout(3,2));
-		
-		
+		contentPane.setLayout(new GridLayout(3, 2, 600, 0));
 		contentPane.add(new JButton("Java File Upload"));
 		contentPane.add(new JButton("Compile"));
 		contentPane.add(new JButton("Run Program"));
 		contentPane.add(new JButton("Compile Error List"));
 		contentPane.add(new JButton("Reset"));
 		contentPane.add(new JButton("Exit"));
+		
 		setSize(1000, 500);
 		setVisible(true);
 	}
