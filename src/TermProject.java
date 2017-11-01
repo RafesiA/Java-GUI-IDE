deceaa0df0527f2c1999897762385992bf194a40
Ecpros <ggwp1486555@gmail.com>
2017-11-01 16:43:33 +0900

    Listener 기능만 약간 구현
    Exit 작동

--- src/TermProject.java
+++ src/TermProject.java
@@ -2,70 +2,70 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class TermProject extends JFrame {
-	public TermProject() {
+
+public class TermProject1 extends JFrame {
+	
+	JButton btn1 = new JButton("Java File Upload");
+	JButton btn2 = new JButton("Compile");
+	JButton btn3 = new JButton("Run Program");
+	JButton btn4 = new JButton("Compile Error List");
+	JButton btn5 = new JButton("Reset");
+	JButton btn6 = new JButton("Exit");
+	
+	public TermProject1() {
 		setTitle("Java IDE");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container contentPane = getContentPane();
 		contentPane.setBackground(Color.WHITE);
-		contentPane.setLayout(new FlowLayout());
+		contentPane.setLayout(new GridLayout(3, 2, 600, 0));
+		MyActionListener al = new MyActionListener();
+		contentPane.add(btn1);
+		btn1.addActionListener(al);
+		contentPane.add(btn2);
+		btn2.addActionListener(al);
+		contentPane.add(btn3);
+		btn3.addActionListener(al);
+		contentPane.add(btn4);
+		btn4.addActionListener(al);
+		contentPane.add(btn5);
+		btn5.addActionListener(al);
+		contentPane.add(btn6);
+		btn6.addActionListener(al);
 		
 		
-		JButton bt1 = new JButton("Java File Upload");
-		JButton bt2 = new JButton("Compile");
-		JButton bt3 = new JButton("Run");
-		JButton bt4 = new JButton("Reset");
-		JButton bt5 = new JButton("Compile Error List");
-		JButton bt6 = new JButton("Exit");
-
-		bt1.addActionListener(new MyActionListener());
-		bt2.addActionListener(new MyActionListener());
-		bt3.addActionListener(new MyActionListener());
-		bt4.addActionListener(new MyActionListener());
-		bt5.addActionListener(new MyActionListener());
-		bt6.addActionListener(new MyActionListener());
 		
-		contentPane.add(bt1);
-		contentPane.add(bt2);
-		contentPane.add(bt3);
-		contentPane.add(bt4);
-		contentPane.add(bt5);
-		contentPane.add(bt6);
-		
-		setSize(350, 150);
+		setSize(1000, 500);
 		setVisible(true);
+	}
+	class MyActionListener implements ActionListener{
+
+		
+		public void actionPerformed(ActionEvent e) {
+			JButton b = (JButton)e.getSource();
+			if(b.getText().equals("Java File Upload"))
+				;
+				//void UploadJ();
+			else if(b.getText().equals("Comile"))
+				;
+				//void Compile();
+			else if(b.getText().equals("Run"))
+				;
+			    //void Run();
+			else if(b.getText().equals("Compile Error List"))
+				;
+			    //void Compile_E();
+			else if(b.getText().equals("Reset"))
+				;
+			    //void Reset();
+			else if(b.getText().equals("Exit"))
+				System.exit(0);
+			    		
+		}
+		
 	}
 	
 	public static void main(String[] args) {
-		TermProject frame = new TermProject();
+		new TermProject1();
 	}
 
-}
-
-class MyActionListener implements ActionListener{
-	public void actionPerformed(ActionEvent e) {
-		JButton b = (JButton)e.getSource();
-		
-		
-		
-		
-		if(b.getText().equals("Java File Upload")) {
-			b.setText("액션 리스너 성공");
-		}
-		else if(b.getText().equals("Compile")) {
-			b.setText("액션 리스너 또한 성공");
-		}
-		else if(b.getText().equals("Run")) {
-			b.setText("Run 성공함");
-		}
-		else if(b.getText().equals("Reset")) {
-			b.setText("초기화 되었음");
-		}
-		else if(b.getText().equals("Compile Error List")){
-			b.setText("CEL이 TextArea에 출력됨");
-		}
-		else if(b.getText().equals("Exit")) {
-			System.exit(-1);
-		}
-	}
 }
